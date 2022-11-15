26629d821c1a8925335b5bafd50950f83117905c
hini1022 <himaja.nimmala@netcracker.com>
2022-11-15 12:00:32 +0530

    java practice task1-7

---
+++ src/main/java/Main.java
@@ -0,0 +1,66 @@
+import org.example.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("****TASK1*****");
+        Task1 t1=new Task1();
+        double[] a=t1.randnum();
+        double min=t1.minarr(a);
+        double max=t1.maxarr(a);
+        double avg=t1.avgarr(a);
+
+        t1.display(a);
+        System.out.println("minimum value in array="+min);
+        System.out.println("maximum value in array="+max);
+        System.out.println("average value in array="+avg);
+
+
+        System.out.println("\n****TASK2*****");
+        double[] bubblesort=t1.bubbesort(a);
+        System.out.println("Bubble sort of an array");
+        for(int i=0;i< bubblesort.length;i++)
+        {
+            System.out.println(bubblesort[i]);
+        }
+        System.out.println("\n****TASK3*****");
+        System.out.println("Prime number in range[0,200]");
+        int[] arr=new int[201];
+        for(int i=0;i<=200;i++)
+        {
+            arr[i]=i;
+
+        }
+        t1.prime(arr);
+        System.out.println("\n****TASK4*****");
+        int[] arr2=new int[10];
+        int[] arr3=new int[10];
+        arr2= new int[]{1, 2, 3, 3,4,5,6,7,8,2,1};
+        arr3=t1.remove(3,arr2);
+        System.out.println("Removal array");
+        for(int i=0;i< arr3.length-1;i++)
+        {
+            System.out.println(arr3[i]);
+        }
+        System.out.println("\n****TASK5*****");
+     String s="himaja";
+     t1.CountLetters(s);
+     System.out.println("\n****TASK6*****");
+     System.out.println("Iterator over the Integer array");
+     t1.iter(arr2);
+
+     System.out.println("\n****TASK7*****");
+     String st="himajahim";
+     char unique=t1.countunique(st);
+        if (unique == '0')
+        {
+            System.out.println("No unique element in string");
+
+        }
+        else {
+            System.out.println("Unique character in the string:" + unique);
+        }
+
+
+    }
+}
---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,158 @@
+package org.example;
+
+
+public class Task1 {
+    public double[] randnum()
+    {
+        double[] arr = new double[10];
+        for(int i=0;i< arr.length;i++) {
+            arr[i] = Math.random();
+        }
+
+        return arr;
+    }
+    public double minarr(double[] arr)
+    {
+        double min=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+    public double maxarr(double[] arr)
+    {
+        double max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    public void display(double[] a)
+    {
+        for(int i=0;i< a.length;i++) {
+            System.out.println(a[i]);
+        }
+    }
+    public double avgarr(double[] a)
+    {
+        double sum=0.0;
+        for(int i=0;i<a.length;i++)
+        {
+            sum=sum+a[i];
+        }
+        double avg= sum/a.length;
+        return avg;
+    }
+    public double[] bubbesort(double[] arr)
+    {
+        double t;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                   t=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=t;
+                }
+            }
+        }
+        return arr;
+    }
+    public void prime(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+//            System.out.println(arr[i]);
+            int j,m=0,flag=0;
+            m=arr[i]/2;
+            if(arr[i]==0||arr[i]==1){
+
+            }
+            else{
+                for(j=2;j<=m;j++){
+                    if(arr[i]%j==0){
+
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0)  {
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+    public int[] remove(int a,int[] arr)
+    {
+
+        int[] arr2 = new int[arr.length - 1];
+        for (int i = 0,j=0; i < arr.length; i++) {
+
+            if (arr[i] == a) {
+                continue;
+            }
+            arr2[j++] = arr[i];
+        }
+     return arr2;
+    }
+
+    public void CountLetters(String s) {
+        String s1="";
+       for(int i=0;i<s.length();i++)
+       {
+//           if(!s1.contains(s.charAt(i))) {
+               int c = 0;
+               for (int j = 0; j < s.length(); j++) {
+                   if (s.charAt(i) == s.charAt(j)) {
+                       c = c + 1;
+                       s1 = s1 + s.charAt(i);
+                   }
+               }
+               System.out.println("the letter " + s.charAt(i) + " repeats " + c +" times");
+           //}
+
+       }
+
+    }
+
+    public void iter(int[] arr2) {
+        for(int i=0;i<arr2.length;i=i+2)
+        {
+            System.out.println(arr2[i]);
+        }
+    }
+
+    public char countunique(String st) {
+        for(int i=0;i<st.length();i++)
+        {
+//           if(!s1.contains(s.charAt(i))) {
+            int c = 0;
+            for (int j = 0; j < st.length(); j++) {
+
+                if (st.charAt(i) == st.charAt(j)) {
+                    c = c + 1;
+
+                }
+            }
+            if(c==1)
+            {
+                return st.charAt(i);
+            }
+           // System.out.println("the letter " + st.charAt(i) + " repeats " + c +" times");
+            //}
+
+        }
+        return '0';
+
+    }
+}
